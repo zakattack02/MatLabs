631963e02449c021a1b1f21c2c20c71ca647db59
Zak Konik <44588781+zakattack02@users.noreply.github.com>
2022-02-09 15:11:28 -0500

    Delete cone.m

--- cone.m
+++
@@ -1,7 +0,0 @@
-clear; clc           %clear workspace 
-r=4;                 % radius of cone 
-h=10;                % Hieght of cone 
-v = (pi*r^2*h)/3    % Volume of cone
-
-%% Cell 02
-a_cyl = pi*r^2*h % Volume of Cyl
